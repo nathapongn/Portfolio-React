11e6862c6db13a935cd6364dc1cdbe53b8700175
Nathapong <101572359+nathapongn@users.noreply.github.com>
2025-11-21 13:24:21 +0700

    ButtonIcon update

--- react-portfolio/src/components/ButtonIcon/ButtonIcon.jsx
+++ react-portfolio/src/components/ButtonIcon/ButtonIcon.jsx
@@ -1,7 +1,20 @@
 import './ButtonIcon.css';
+import { useState } from 'react';
 
-export default function ButtonIcon({className, children, onClick}) {
+export default function ButtonIcon({className, children, onClick, hoveredIcon}) {
+    const [ hovered, setHovered ] = useState(false)
+
+    // Show hoveredIcon when hovered, fallback to children; otherwise show children
+    const Icon = hovered ? ( hoveredIcon || children ) : children;
+
     return(
-        <button className={`button-icon icon-enabled ${className}`}onClick={onClick}>{children}</button>
+        <button 
+            className={`button-icon icon-enabled ${className}`} 
+            onClick={onClick} 
+            onMouseEnter={() => setHovered(true)} 
+            onMouseLeave={() => setHovered(false)}
+            >
+            {Icon}
+            </button>
     )
 }
